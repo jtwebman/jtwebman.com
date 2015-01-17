5edd499ba433d03d1baea9be91f4b09b002c8e8a
Jeff Turner <jtwebman@gmail.com>
2015-01-17 01:11:57 -0800

    Added Travis-CI config and a single test.

---
+++ test/collections.js
@@ -0,0 +1,9 @@
+var should = require('chai').should();
+
+describe('Docpad', function () {
+  describe('collections', function(){
+    it('should have a pages collection', function(){
+      
+    });
+  });
+});
