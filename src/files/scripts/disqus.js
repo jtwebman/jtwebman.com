bfe6550a4ce33dde394c9d2a8a01fce689542148
Jeff Turner <jtwebman@gmail.com>
2015-02-16 11:18:26 -0800

    Cloudflare Rocket Loader Fix 2

--- src/files/scripts/disqus.js
+++ src/files/scripts/disqus.js
@@ -3,9 +3,9 @@ var disqus_shortname = 'jtwebman';
 (function () {
   'use strict';
   var dsq = document.createElement('script');
+  dsq.setAttribute('data-cfasync', 'false');
   dsq.type = 'text/javascript';
   dsq.async = true;
   dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-  dsq.setAttribute('data-cfasync', 'false');
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 }());
