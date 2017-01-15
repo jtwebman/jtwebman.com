edae9d6143f5950423ee20212bf5b9098eb09d66
JT Turner <jtwebman@gmail.com>
2017-01-15 10:57:17 -0800

    Remove Docpad for starting with Hexo.

--- src/documents/videos.html.md
+++
@@ -1,9 +0,0 @@
----
-title: "Videos"
-layout: "page"
-sort: 3
----
-
-# My Videos
-
-For now just go over to my [Youtube](https://www.youtube.com/jtwebman). I eventually will pull these into Docpad.
--- test/collections.js
+++
@@ -1,9 +0,0 @@
-var should = require('chai').should();
-
-describe('Docpad', function () {
-  describe('collections', function(){
-    it('should have a pages collection', function(){
-
-    });
-  });
-});
